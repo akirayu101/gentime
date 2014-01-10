2a37008632d3b187283c860034ec16163358549a
yuxin05 <yuxin05@baidu.com>
2014-01-10 23:05:36 +0800

    add operators

--- src/operators.py
+++ src/operators.py
@@ -9,9 +9,21 @@ def error_format_filter(text,lang):
 
 def contain_filter(text,lang):
     contain_dict = getattr(process_dict,lang+'_contain_dict')
-    print contain_dict
-
     for contain_seg in contain_dict:
         if contain_seg in text.split('\t')[0]:
+            return True,text
+    return False,text
+
+def punish_filter(text,lang):
+    punish_dict = getattr(process_dict,lang+'_punish_dict')
+    for punish_seg in punish_dict:
+        if punish_seg in text.split('\t')[0]:
+            return False,text
+    return True,text
+
+def prefix_filter(text,lang):
+    prefix_dict = getattr(process_dict,lang+'_prefix_dict')
+    for prefix_seg in prefix_dict:
+        if text.split('\t')[0].startswith(prefix_seg):
             return True,text
     return False,text
