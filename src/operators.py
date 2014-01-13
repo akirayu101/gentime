5aff2e4251ed1fdf9821f0022c606fdcb48b3fb9
yuxin05 <yuxin05@baidu.com>
2014-01-13 15:23:09 +0800

    add stem extrator

--- src/operators.py
+++ src/operators.py
@@ -1,6 +1,11 @@
 #coding=utf-8
 import process_dict
+import copy
 
+//过滤器operator
+//返回bool
+//in query freq
+//out query freq
 def error_format_filter(text,lang):
     if len(text.split('\t')) == 2:
         try:
@@ -33,3 +38,26 @@ def prefix_filter(text,lang):
         if text.split('\t')[0].startswith(prefix_seg):
             return True,text
     return False,text
+
+
+//主干提取operator
+//全返回true和text
+
+//in query \t freq
+//out query \t stem \t freq
+
+def stem_extractor(text,lang):
+    stem_dict = getattr(process_dict,lang+'_stem_dict')
+    [query,freq] = text.strip().split('\t')
+    zhugan = copy.deepcopy(query)
+
+    for stem in stem_dict:
+        stem_index = query.find(stem)
+        if stem_index != -1:
+            zhugan = copy.deepcopy(query)
+            query[stem_index,stem_index+len(stem)] = ''
+            return True,'\t'.join([zhugan,query,freq])+'\n'
+    //impossible to reach this
+    return False,text
+
+
