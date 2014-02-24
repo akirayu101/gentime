769347d560404b1656aac3360c85bdebc90bab2f
yuxin05 <yuxin05@baidu.com>
2014-02-24 17:26:55 +0800

    add sample tool

--- src/sample.py
+++ src/sample.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 """
 Usage:
-      sample.py <infile>
+      sample.py <infile> <mode>
 """
 
 import random
 from docopt import docopt
 
 
-def get_sample(in_file):
+def get_sample(in_file, mode):
     lines = []
     with open(in_file) as f:
         for line in f:
@@ -16,10 +16,14 @@ def get_sample(in_file):
 
     lines_num = len(lines)
     sample = [lines[i] for i in random.sample(xrange(1, lines_num), 1000)]
-    for line in sample:
-        print '\t'.join(line.strip().split('\t')[:2])
+    if mode == "recall":
+        for line in sample:
+            print '\t'.join(line.strip().split('\t')[:2])
+    elif mode == "stem":
+        for line in sample:
+            print line.strip().split('\t')[0]
 
 
 if __name__ == "__main__":
     arguments = docopt(__doc__)
-    get_sample(arguments['<infile>'])
+    get_sample(arguments['<infile>'], arguments['<mode>'])
