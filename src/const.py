b84a9f885df129705647b2f4cbed494a4b3bcc38
yuxin05 <yuxin05@baidu.com>
2014-04-14 11:53:36 +0800

    modify const

--- src/const.py
+++ src/const.py
@@ -4,6 +4,7 @@ mid_datadir = '../mid_data/'
 final_datadir = '../final_data/'
 dict_dir = '../dict/'
 file_suffix = when.now().strftime("%Y%m%d")
+newest_time = when.past(0, 3).strftime("%Y%m%d")
 
-gentime_type = {'year': ['1', '0'], 'newest': ['2', '0'],
+gentime_type = {'year': ['1', '0'], 'newest': ['3', newest_time],
                 '2014': ['2', '0'], '2013': ['3', '20130101']}
